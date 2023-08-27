a3ed5bfa4299ac9a32ab53c4c4d121c72ed1294b
Darwin <135076755+2darwin@users.noreply.github.com>
2023-08-27 11:13:26 -0400

    Made a function that takes user input

--- script.js
+++ script.js
@@ -6,15 +6,20 @@ function Book(title, author, pages, isRead) {
     this.author = author
     this.pages = pages
     this.isRead = isRead
-    this.info = function() {
-        console.log(title, author, pages, isRead)
-    }
+    this.year = year
 }
 
 function addBookToLibrary() {
-
+    const newBook = new Book(title, author, pages, isRead, year);
+    myLibrary.push(newBook);
 }
 
-const theHobbit = new Book('The Hobbit', 'by J.R.R. Tolkien,', '295 pages,', 'not read yet')
+const title = prompt("Enter title of the book:");
+const author = prompt("Enter author of the book:");
+const pages = prompt("Enter how many pages the book has:");
+const isRead = prompt("Have you read the book?:");
+const year = prompt("What year did the book come out?:");
 
-theHobbit.info() //The Hobbit by J.R.R. Tolkien, 295 pages, not read yet
+console.log(addBookToLibrary(title, author, pages, isRead, year));
+console.log(myLibrary);
+//const theHobbit = new Book('The Hobbit', 'by J.R.R. Tolkien,', '295 pages,', 'not read yet', '1937')
