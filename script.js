9c65909f2fc7282e0f8fd98c359277721082aae0
Darwin <135076755+2darwin@users.noreply.github.com>
2023-09-05 19:26:07 -0400

    Added button that shows books added

--- script.js
+++ script.js
@@ -10,34 +10,37 @@ function Book(title, author, pages, isRead, year) {
 }
 
 function addBookToLibrary(title, author, pages, isRead, year) {
-    const newBook = new Book(title, author, pages, isRead, year);
+    let newBook = new Book(title, author, pages, isRead, year);
     myLibrary.push(newBook);
 }
 
 
-
 // Manually add some books to the library for demonstration
-addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 234, "yes", 1951);
-addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, "no", 1960);
-addBookToLibrary("1984", "George Orwell", 328, "yes", 1949);
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "no", 1937);
+addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", "234", "yes", "1951");
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", "281", "no", "1960");
+addBookToLibrary("1984", "George Orwell", "328", "yes", "1949");
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", "no", "1937");
+addBookToLibrary("A Game of Thrones", "George R.R. Martin", "694", "no", "1996");
+addBookToLibrary("The Maze Runner", "James Dashner", "375", "yes", "2009");
 
 function displayBooks() {
-  const bookContainer = document.getElementById("book-container");
+  let bookContainer = document.getElementById("book-container");
   bookContainer.innerHTML = ""; // Clear previous content
 
-  myLibrary.forEach(book => {
-    const bookCard = document.createElement("div");
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+
+    let bookCard = document.createElement("div");
     bookCard.className = "book-card";
     bookCard.innerHTML = `
-      <h2>${book.title}</h2>
-      <p>Author: ${book.author}</p>
-      <p>Pages: ${book.pages}</p>
-      <p>Read: ${book.isRead}</p>
-      <p>Year: ${book.year}</p>
+    <h2>${book.title}</h2>
+    <p>Author: ${book.author}</p>
+    <p>Pages: ${book.pages}</p>
+    <p>Read: ${book.isRead}</p>
+    <p>Year: ${book.year}</p>
     `;
     bookContainer.appendChild(bookCard);
-  });
+  }
 }
 
 // Call the displayBooks function to show the books on the page
@@ -47,6 +50,11 @@ document.addEventListener('DOMContentLoaded', function() {
   let showFormButton = document.getElementById('showFormButton');
   let bookForm = document.getElementById('bookForm');
   let newBookForm = document.getElementById('newBookForm');
+  let refresh = document.getElementById('refresh');
+
+  refresh.addEventListener('click', function() {
+    displayBooks();
+  });
 
   showFormButton.addEventListener('click', function() {
     bookForm.style.display = 'block';
@@ -61,12 +69,10 @@ document.addEventListener('DOMContentLoaded', function() {
     let isRead = document.getElementById('isRead').value;
     let year = document.getElementById('year').value;
 
+    addBookToLibrary(title, author, pages, isRead, year);
+
     console.log("New Book Details:");
-    console.log("Title:", title);
-    console.log("Author:", author);
-    console.log("Pages:", pages);
-    console.log("Read?:", isRead);
-    console.log("Year:", year);
+    console.table(myLibrary);
 
     //Reset the form and hide it
     newBookForm.reset();
